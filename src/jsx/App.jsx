e6ff6064012700ac7c564cbeb0d22d1fa7dc8414
Teemo Tebest <teemo.tebest@gmail.com>
2021-06-02 11:21:37 +0200

    Updated dates and gifs/jpgs and added confetti

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import style from './../styles/styles.less';
 
+import Confetti from 'react-confetti';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,11 +27,13 @@ class App extends Component {
   render() {
     return (
       <div className={style.app}>
+        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={true} className={style.rain} />
         <div className={style.image_container}><img src="img/mexican.gif" /></div>
         <h3>HOMBRES AND AMIGO&shy;RETTES!</h3>
         <h1>FISH TACO NIGHT</h1>
-        <div className={style.image_container}><img src="img/taco1.gif" /></div>
-        <h3>VIVA This Friday!</h3>
+        <div className={style.image_container}><img src="img/taco4.gif" /></div>
+        <h3>VIVA This Saturday!</h3>
+        <div className={style.image_container}><img src="img/taco3.jpg" /></div>
         <h1>Vamos 19:30!</h1>
         <div className={style.image_container}><img src="img/taco2.gif" /></div>
       </div>
