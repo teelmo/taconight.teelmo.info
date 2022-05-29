45304da1a105426a1cb0fdf863f5871412f201f5
Teemo Tebest <teemo.tebest@gmail.com>
2022-05-29 13:55:03 +0200

    Updated

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -11,13 +11,43 @@ class App extends Component {
     }
   }
   componentDidMount() {
-
+    this.counter();
   }
   componentDidUpdate(prevProps, prevState, snapshot) {
 
   }
   componentWillUnMount() {
 
+  }
+  counter() {
+    // The data/time we want to countdown to
+    var countDownDate = new Date('May 29, 2022 18:30:00').getTime();
+
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+      var now = new Date().getTime();
+      var timeleft = countDownDate - now;
+          
+      // Calculating the days, hours, minutes and seconds left
+      var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+          
+      // Result is output to the specific element
+      document.getElementById('hours').innerHTML = hours + 'h ' 
+      document.getElementById('mins').innerHTML = minutes + 'm ' 
+      document.getElementById('secs').innerHTML = seconds + 's ' 
+          
+      // Display the message when countdown is over
+      if (timeleft < 0) {
+          clearInterval(myfunc);
+          document.getElementById('hours').innerHTML = '' 
+          document.getElementById('mins').innerHTML = ''
+          document.getElementById('secs').innerHTML = ''
+          document.getElementById('end').innerHTML = 'YOU ARE LATE!!';
+      }
+    }, 1000);
   }
   // shouldComponentUpdate(nextProps, nextState) {}
   // static getDerivedStateFromProps(props, state) {}
@@ -31,11 +61,13 @@ class App extends Component {
         <div className={style.image_container}><img src="img/mexican.gif" /></div>
         <h3>HOMBRES AND AMIGO&shy;RETTES!</h3>
         <h1>FISH TACO NIGHT</h1>
+        <h3>Vamos!</h3>
         <div className={style.image_container}><img src="img/taco4.gif" /></div>
-        <h3>VIVA This Saturday!</h3>
+        <h3>VIVA no mañana sino HOY!</h3>
         <div className={style.image_container}><img src="img/taco3.jpg" /></div>
-        <h1>Vamos 18:29!</h1>
+        <h1>You still have <span id="hours"></span><span id="mins"></span><span id="secs"></span><span id="end"></span> until the Montezuma's Revenge</h1>
         <div className={style.image_container}><img src="img/taco2.gif" /></div>
+        <h3>Finnish world championship sideshow!</h3>
       </div>
     );
   }
