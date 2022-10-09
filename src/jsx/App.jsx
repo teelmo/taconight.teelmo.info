1122cdf44879a38f38cb42ccadf7a2981a0251ba
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-09 21:57:45 +0200

    Updated webpack

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,75 +1,63 @@
-import React, {Component} from 'react';
-import style from './../styles/styles.less';
+import React, { useEffect } from 'react';
+import '../styles/styles.less';
 
 import Confetti from 'react-confetti';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    }
-  }
-  componentDidMount() {
-    this.counter();
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-
-  }
-  componentWillUnMount() {
-
-  }
-  counter() {
+function App() {
+  const counter = () => {
     // The data/time we want to countdown to
-    var countDownDate = new Date('May 29, 2022 18:30:00').getTime();
+    const countDownDate = new Date('May 29, 2022 18:30:00').getTime();
 
     // Run myfunc every second
-    var myfunc = setInterval(function() {
-      var now = new Date().getTime();
-      var timeleft = countDownDate - now;
-          
+    const myfunc = setInterval(() => {
+      const now = new Date().getTime();
+      const timeleft = countDownDate - now;
+
       // Calculating the days, hours, minutes and seconds left
-      var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
-          
+      // const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
       // Result is output to the specific element
-      document.getElementById('hours').innerHTML = hours + 'h ' 
-      document.getElementById('mins').innerHTML = minutes + 'm ' 
-      document.getElementById('secs').innerHTML = seconds + 's ' 
-          
+      document.getElementById('hours').innerHTML = `${hours}h `;
+      document.getElementById('mins').innerHTML = `${minutes}m `;
+      document.getElementById('secs').innerHTML = `${seconds}s `;
+
       // Display the message when countdown is over
       if (timeleft < 0) {
         clearInterval(myfunc);
-        document.getElementById('hours').innerHTML = '' 
-        document.getElementById('mins').innerHTML = ''
-        document.getElementById('secs').innerHTML = ''
+        document.getElementById('hours').innerHTML = '';
+        document.getElementById('mins').innerHTML = '';
+        document.getElementById('secs').innerHTML = '';
         document.getElementById('message').innerHTML = 'YOU ARE LATE OR PRESENT!!';
       }
     }, 1000);
-  }
-  // shouldComponentUpdate(nextProps, nextState) {}
-  // static getDerivedStateFromProps(props, state) {}
-  // getSnapshotBeforeUpdate(prevProps, prevState) {}
-  // static getDerivedStateFromError(error) {}
-  // componentDidCatch() {}
-  render() {
-    return (
-      <div className={style.app}>
-        <Confetti width={window.innerWidth} height={window.innerHeight} recycle={true} className={style.rain} />
-        <div className={style.image_container}><img src="img/mexican.gif" /></div>
-        <h3>HOMBRES AND AMIGO&shy;RETTES!</h3>
-        <h1>FISH TACO NIGHT</h1>
-        <h3>Vamos!</h3>
-        <div className={style.image_container}><img src="img/taco4.gif" /></div>
-        <h3>VIVA no mañana sino HOY!</h3>
-        <div className={style.image_container}><img src="img/taco3.jpg" /></div>
-        <h1 id="message">You still have <span id="hours"></span><span id="mins"></span><span id="secs"></span> until the Montezuma's Revenge</h1>
-        <div className={style.image_container}><img src="img/taco2.gif" /></div>
-        <h3>Finnish world championship sideshow!</h3>
-      </div>
-    );
-  }
+  };
+  useEffect(() => {
+    counter();
+  }, []);
+  return (
+    <div className="app">
+      <Confetti width={window.innerWidth} height={window.innerHeight} recycle className="rain" />
+      <div className="image_container"><img src="./assets/img/mexican.gif" alt="Mexican meme" /></div>
+      <h3>HOMBRES AND AMIGO&shy;RETTES!</h3>
+      <h1>FISH TACO NIGHT</h1>
+      <h3>Vamos!</h3>
+      <div className="image_container"><img src="./assets/img/taco4.gif" alt="Mexican meme" /></div>
+      <h3>VIVA no mañana sino HOY!</h3>
+      <div className="image_container"><img src="./assets/img/taco3.jpg" alt="Mexican meme" /></div>
+      <h1 id="message">
+        You still have
+        <span id="hours" />
+        <span id="mins" />
+        <span id="secs" />
+        {' '}
+        until the Montezuma&apos;s Revenge
+      </h1>
+      <div className="image_container"><img src="./assets/img/taco2.gif" alt="Mexican meme" /></div>
+      <h3>Finnish world championship sideshow!</h3>
+    </div>
+  );
 }
 export default App;
